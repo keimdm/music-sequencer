89a174205d9b323583480f0245b093e60c44c37c
David Keim <dmk252@cornell.edu>
2023-02-03 22:39:57 -0500

    added control to change number of columns

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,12 @@
 var columnClicked = 0;
 var rowClicked = 0;
+var columnsEntered = 0;
+
+let columnCount = document.getElementById("columnCount");
+
+columnCount.addEventListener('change', (event) => {
+    columnChange();
+})
 
 function columnClick(x) {
     columnClicked = x;
@@ -11,4 +18,22 @@ function columnClick(x) {
 
 function cellClick(x) {
     rowClicked = x;
+}
+
+function columnChange() {
+    columnsEntered = Number(document.getElementById("columnCount").value);
+    if (columnsEntered > 32) {
+        columnsEntered = 32;
+        document.getElementById("columnCount").value = 32;
+    }
+    if (columnsEntered < 1) {
+        columnsEntered = 1;
+        document.getElementById("columnCount").value = 1
+    }
+    for (i = 1; i < columnsEntered + 1; i++) {
+        document.getElementById("column" + i.toString()).style.display = "inline-flex";
+    }
+    for (j = columnsEntered + 1; j < 33; j++) {
+        document.getElementById("column" + j.toString()).style.display = "none";
+    }
 }

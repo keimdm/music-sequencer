9db222efa0776da39846c569ed840158af429597
David Keim <dmk252@cornell.edu>
2023-02-03 21:29:24 -0500

    interface and HTML updates

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,14 @@
-function cellClick() {
-    console.log("cell clicked!");
+var columnClicked = 0;
+var rowClicked = 0;
+
+function columnClick(x) {
+    columnClicked = x;
+    if (rowClicked != 0 && columnClicked != 0) {
+        console.log("Column: " + columnClicked.toString() + ", " + "Row: " + rowClicked.toString());
+    }
+    console.log
+}
+
+function cellClick(x) {
+    rowClicked = x;
 }
